ee00987ffe33f0f33e9469aaf6243cf36043507a
laskarina <a.toki0117@gmail.com>
2020-05-28 18:54:36 +0900

    abc166

---
+++ abc166/a/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    string A;
+    cin >> A;
+
+    if(A == "ABC"){
+        cout << "ARC" << endl;
+    }else{
+        cout << "ABC" << endl;
+    }
+
+}
